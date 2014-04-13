8ae6a784d4083fe0ef4d307053c0b107dc69423a
Sam Stiles <sam@phasesolutions.ca>
2014-04-13 19:29:06 -0300

    Frontend for login done, prepping angular controllers

--- .tmp/public/js/controllers.js
+++ .tmp/public/js/controllers.js
@@ -1,11 +1,19 @@
-productivityTime.controller('tasksController', function($scope){
+productivityTime.controller('tasksController', function($scope, $rootScope){
 
 });
 
-productivityTime.controller('authLoginController', function($scope){
+productivityTime.controller('authLoginController', function($scope, $rootScope){
+
+  $scope.email_signin = "";
+  $scope.password_signin = "";
+
+  angular.element(document.querySelector('#login')).on('click', function(){
+    console.log(JSON.stringify($scope.email_signin, null, 4));
+    console.log(JSON.stringify($scope.password_signin, null, 4));
+  });
 
 });
 
-productivityTime.controller('authLogoutController', function($scope){
+productivityTime.controller('authLogoutController', function($scope, $rootScope){
 
 });
